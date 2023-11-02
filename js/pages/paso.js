4ed66aa6a10dab244fd077ea2c97839e2ea577ca
marcostamburi <marcose027@hotmail.com>
2023-11-01 21:24:20 -0300

    .

--- js/pages/paso.js
+++ js/pages/paso.js
@@ -1,17 +1,161 @@
 const tipoEleccion = 1;
+
 const tipoRecuento = 1;
 
-async function solicitarDatosApi() {
+const periodosSelect = document.getElementById("anio");
+const cargosSelect = document.getElementById("cargo");
+const distritosSelect = document.getElementById("distrito");
+const seccionSelect = document.getElementById("seccion");
+let seccionesTotales = []
+
+async function cargarAños() {
+    const response = await fetch("https://resultados.mininterior.gob.ar/api/menu/periodos");
+    const años = await response.json();
+
+    años.forEach(año => {
+        const option = document.createElement("option");
+        option.value = año;
+        option.text = año;
+        periodosSelect.appendChild(option);
+    });
+}
+
+
+async function cargarCargos() {
+    const añoSeleccionado = periodosSelect.value;
+
+    if (añoSeleccionado) {
+        const response = await fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + añoSeleccionado);
+        const datos = await response.json();
+
+        cargosSelect.innerHTML = '<option value="">Cargo</option>';
+
+        datos.forEach(dato => {
+            dato.Cargos.forEach(cargo => {
+                const option = document.createElement("option");
+                option.value = cargo.IdCargo;
+                option.text = cargo.Cargo;
+                cargosSelect.appendChild(option);
+            });
+        });
+    }
+}
+
+
+async function cargarDistritos() {
+    const cargoSeleccionado = cargosSelect.value;
+
+    if (cargoSeleccionado) {
+        const response = await fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + periodosSelect.value);
+        const datos = await response.json();
+
+        distritosSelect.innerHTML = '<option value="">Distrito</option>';
+
+        datos.forEach(dato => {
+            dato.Cargos.forEach(cargo => {
+                if (cargo.IdCargo === cargoSeleccionado) {
+                    cargo.Distritos.forEach(distrito => {
+                        const option = document.createElement("option");
+                        option.value = distrito.IdDistrito;
+                        option.text = distrito.Distrito;
+                        distritosSelect.appendChild(option);
+                    });
+                }
+            });
+        });
+    }
+}
+
+
+
+async function cargarSecciones() {
+    const distritoSeleccionado = distritosSelect.value;
+    console.log(distritoSeleccionado)
+
+    if (distritoSeleccionado) {
+        const response = await fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + periodosSelect.value);
+        const datos = await response.json();
+
+        seccionSelect.innerHTML = '<option value="">Sección</option>';
+
+        datos.forEach(dato => {
+            dato.Distrito.forEach(distrito => {
+                if (distrito.IdDistrito === distritoSeleccionado) {
+                    distrito.SeccionProvincial.forEach(seccionProvincial => {
+                        seccionProvincial.Secciones.forEach(seccion =>{
+                        const option = document.createElement("option");
+                        option.value = seccion.IdSeccionProvincial;
+                        option.text = seccion.Seccion;
+                        seccionSelect.appendChild(option);} );
+                        
+                    });
+                }
+            });
+        });
+    }
+}
+
+
+
+periodosSelect.addEventListener("change", cargarCargos);
+cargosSelect.addEventListener("change", cargarDistritos);
+distritosSelect.addEventListener("change", cargarSecciones);
+
+
+cargarAños();
+
+
+
+
+
+
+
+
+const Anio = document.getElementById("anio");
+const Cargo = document.getElementById("cargo");
+const Distrito = document.getElementById("distrito");
+const Seccion = document.getElementById("seccion");
+
+//Tambien podemos usar Onload para que se actualize al seleccionar todos los campos
+//Solo se actualizara al seleccionar todos
+//Si usamos onchange al seleccionar el año el subtitulo de color azul nos muestra el indice 0 en todos los campos incompletos
+//Ej: 2019 > Paso > 0 > 0 > 0 > 
+
+Anio.addEventListener("change", actualizarDatos);
+Cargo.addEventListener("change", actualizarDatos);
+Distrito.addEventListener("change", actualizarDatos);
+Seccion.addEventListener("change", actualizarDatos);
+
+
+function actualizarDatos() {
+
+    const anio = Anio.value;
+    const tipoEleccion = "Paso";
+    const cargo = Cargo.value;
+    const distrito = Distrito.value;
+    const seccion = Seccion.value;
+
+
+    const titulo = document.getElementById("titulo");
+    const subtitulo = document.getElementById("subtitulo");
+
+    titulo.innerText = `Elecciones ${anio} | ${tipoEleccion}`;
+    subtitulo.innerText = `${anio} > ${tipoEleccion} > ${cargo} > ${distrito} > ${seccion}`;
+}
+
+
+
+/*async function solicitarDatosApi() {
     try { 
         const response = await fetch("https://resultados.mininterior.gob.ar/api/menu/periodos");
         if (!response.ok) {
             throw new Error("Error en la solicitud");
         }
         const años = await response.json();
-        return años; //Devolvemos los años de la api
+        return años; 
     } catch (error) {
         console.error("Error en fetchDatos:", error);
-        throw error; //Mostramos mensaje de error por si la api no funciona ese dia
+        throw error; 
     }
 }
 
@@ -122,7 +266,7 @@ function mostrarDistritos(arrayDistritos) {
 
     var selectElement = document.getElementById("distrito");
 
-    let selectHTML = '<option value="0">Distrito</option>';
+    let selectHTML = '<option value="Distrito">Distrito</option>';
 
     selectHTML += arrayDistritos.map(distrito => {
         return `<option value="${distrito}">${distrito}</option>`;
@@ -156,7 +300,7 @@ function distritoSeleccionado() {
 function mostrarSecciones(seccionesAMostrar) {
     var selectElement = document.getElementById("seccion");
 
-    let selectHTML = '<option value="0">Sección</option>';
+    let selectHTML = '<option value="Sección">Sección</option>';
 
     selectHTML += seccionesAMostrar.map(seccion => {
         return `<option value="${seccion.IdSeccion}">${seccion.Seccion}</option>`;
@@ -181,41 +325,5 @@ function seccionSeleccionada() {
             console.log(seccionesFiltradas);
         }
     }
-}
-
-
-
-const Anio = document.getElementById("anio");
-const Cargo = document.getElementById("cargo");
-const Distrito = document.getElementById("distrito");
-const Seccion = document.getElementById("seccion");
-
-//Tambien podemos usar Onload para que se actualize al seleccionar todos los campos
-//Solo se actualizara al seleccionar todos
-//Si usamos onchange al seleccionar el año el subtitulo de color azul nos muestra el indice 0 en todos los campos incompletos
-//Ej: 2019 > Paso > 0 > 0 > 0 > 
-
-Anio.addEventListener("change", actualizarDatos);
-Cargo.addEventListener("change", actualizarDatos);
-Distrito.addEventListener("change", actualizarDatos);
-Seccion.addEventListener("change", actualizarDatos);
-
-
-function actualizarDatos() {
-    
-    const anio = Anio.value;
-    const tipoEleccion = "Paso"; 
-    const cargo = Cargo.value;
-    const distrito = Distrito.value;
-    const seccion = Seccion.value;
-
-   
-    const titulo = document.getElementById("titulo");
-    const subtitulo = document.getElementById("subtitulo");
-
-    titulo.innerText = `Elecciones ${anio} | ${tipoEleccion}`;
-    subtitulo.innerText = `${anio} > ${tipoEleccion} > ${cargo} > ${distrito} > ${seccion}`;
-}
-
-
+}*/
 
