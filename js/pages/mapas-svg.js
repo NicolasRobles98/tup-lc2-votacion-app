6666ae5b4521998d0932d19f21d2d5f1715afbb6
Nicolas Robles <nicolasrobles98@gmail.com>
2023-11-08 18:54:43 -0300

    array mapas

---
+++ js/pages/mapas-svg.js
@@ -0,0 +1,25 @@
+const provinciasIds = [
+    "Ciudad Autónoma de Buenos Aires",
+    "Buenos Aires",
+    "Catamarca",
+    "Córdoba",
+    "Corrientes",
+    "Chaco",
+    "Chubut",
+    "Entre Rios",
+    "Formosa",
+    "Jujuy",
+    "La Pampa",
+    "La Rioja",
+    "Mendoza",
+    "Misiones",
+    "Neuquén",
+    "Salta",
+    "San Juan",
+    "San Luis",
+    "Santa Cruz",
+    "Santa Fe",
+    "Santiago del Estero",
+    "Tucumán"
+  ];
+  
